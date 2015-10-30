031eb9c3ce184b6f2a3dfde995153cb81c5a97c9
Simon Robert <simon.robert@virtual-expo.com>
2015-10-30 15:28:55 +0100

    Développement initial + README

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # srob-config
 Configuration loader
+
+## Installation
+
+    npm install srob-config
+
+## Usage
+
+    var configLoad = require('srob-config');
+
+    configLoad.load([
+        // Files list. JS or yml.
+        __dirname + "/path/to/foo.js",
+        "bar.yml",
+    ], [
+        // Strings replacement list.
+        ["{dirname}", __dirname],
+        ["{Foo}", "bar"],
+    ]);
+
+*
---
+++ app.js
@@ -0,0 +1,76 @@
+var yaml = require('js-yaml');
+var fs = require('fs');
+var Q = require('q');
+var _ = require('underscore');
+var extend = require('extend');
+var traverse = require('traverse');
+
+/**
+ * Charge des fichiers de configuration (js, yaml)
+ * @constructor
+ */
+function SrobConfig() {
+    var that = this;
+    var includeKey = "include";
+
+    /**
+     * Charge une liste de fichiers.
+     *
+     * @param files
+     * @param replacements
+     */
+    that.load = function (files, replacements) {
+        var config = {};
+        if (typeof files === "string") {
+            files = [files];
+        }
+
+        var promises = [];
+        _.each(files, function (file) {
+            var p;
+            if (file.match(/\.js$/)) {
+                // Fichier js
+                p = Q(require(file));
+
+            } else {
+                // Fichier Yaml
+                p = Q.nfcall(fs.readFile, file, "utf-8")
+                    .then(function (data) {
+                        return yaml.safeLoad(data);
+                    });
+            }
+
+            p = p.then(function (config) {
+                if (replacements) {
+                    traverse(config).forEach(function (obj) {
+                        if (!this.isLeaf || typeof obj !== "string") return;
+                        _.each(replacements, function (replacement) {
+                            obj = obj.replace(new RegExp(replacement[0], "g"), replacement[1]);
+                        });
+                        this.update(obj);
+                    });
+                }
+
+                if (config[includeKey] !== undefined) {
+                    return that.load(config[includeKey])
+                        .then(function (includedConfig) {
+                            delete config[includeKey];
+                            return extend(true, config, includedConfig);
+                        });
+                }
+
+                return config;
+            });
+
+            promises.push(p);
+        });
+
+        return Q.all(promises)
+            .then(function (configs) {
+                return extend.apply(this, [true, {}].concat(configs));
+            });
+    };
+}
+
+var srobConfig = new SrobConfig();
+module.exports = srobConfig.load;
